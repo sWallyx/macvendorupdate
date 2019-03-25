c4c381a27230fce520aa55f74f86576226e6dbe2
MIKEL <mikelsanchezmart@gmail.com>
2019-03-25 10:11:40 +0100

    Add Setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='macvendorupdate',
+    packages=['macvendorupdate'],
+    version='1',
+    description='Scripts to update the mac address brand list',
+    author='sWallyx',
+    keywords=['mac address', 'wifi', 'brands', 'people'],
+    classifiers=[],
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+)
