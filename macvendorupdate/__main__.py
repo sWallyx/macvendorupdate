c36f366449a4162bea5599fbcbdb6ef0ee77c192
Mikel Sanchez <mikelsmartinez@aol.com>
2020-02-03 12:32:50 +0000

    first cleanaup

--- macvendorupdate/__main__.py
+++ macvendorupdate/__main__.py
@@ -1,20 +1,17 @@
-import re
-import urllib
 import sys
 import os
 import pathlib
-import mysql.connector
 import click
 
 from macvendorToMysql import *
 from macVendorToPython import *
 
-@click.command()
-@click.option('-p','--python', is_flag=True, help='Updates/generates python oui file with mac vendor info')
-@click.option('-m','--mysql', is_flag=True, help='Writes the infor of the mac vendor into the sql database')
 
+@click.command()
+@click.option('-p', '--python', is_flag=True, help='Updates/generates python oui file with mac vendor info')
+@click.option('-m', '--mysql', is_flag=True, help='Writes the infor of the mac vendor into the sql database')
 def main(python, mysql):
-	print('''
+    print('''
     __  ___              _    __               __              __  __          __      __     
    /  |/  /___ ______   | |  / /__  ____  ____/ /___  _____   / / / /___  ____/ /___ _/ /____ 
   / /|_/ / __ `/ ___/   | | / / _ \/ __ \/ __  / __ \/ ___/  / / / / __ \/ __  / __ `/ __/ _ \ 
@@ -23,12 +20,12 @@ def main(python, mysql):
                                                                 /_/                           
      ''')
 
-	if(python):
-		updatePython()
+    if(python):
+        updatePython()
 
-	if(mysql):
-		updateMysql()
+    if(mysql):
+        updateMysql()
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- macvendorupdate/macVendorToPython.py
+++ macvendorupdate/macVendorToPython.py
@@ -6,49 +6,51 @@ import sys
 import os
 import pathlib
 
-global rem_file
-rem_file = "oui.txt"
+global REM_FILE
+REM_FILE = "oui.txt"
+
 
 def dlProgress(count, blockSize, totalSize):
-	percent = int(count*blockSize*100/totalSize)
-	sys.stdout.write("\r" + rem_file + " ========= -> %d%%" % percent)
-	sys.stdout.flush()
+    """
+        Creates a progress bar to indicate the download progress
+    """
+    percent = int(count*blockSize*100/totalSize)
+    sys.stdout.write("\r" + REM_FILE + " ========= -> %d%%" % percent)
+    sys.stdout.flush()
+
 
 def updatePython():
-	# Define variables
-	rem_file = "oui.txt"
-
-	OUI_URL = "http://standards.ieee.org/develop/regauth/oui/oui.txt"
-	OUI_FILE = "oui.txt"
+    OUI_URL = "http://standards.ieee.org/develop/regauth/oui/"+REM_FILE
+    OUI_FILE = "oui.txt"
     # download oui.txt
-	print "Downloading from",OUI_URL
-	urllib.urlretrieve(OUI_URL, OUI_FILE, reporthook=dlProgress)
+    print("Downloading from", OUI_URL)
+    urllib.urlretrieve(OUI_URL, OUI_FILE, reporthook=dlProgress)
 
-	# open file and rewrite
-	f = open('oui.py','w')
-	f.write('# -*- coding: utf-8 -*-\noui = {\n')
+    # open file and rewrite
+    f = open('oui.py', 'w')
+    f.write('# -*- coding: utf-8 -*-\noui = {\n')
 
     # parsing oui.txt data
-	with open(OUI_FILE) as infile:
-		for line in infile:
-		    if re.search("(hex)", line):
-				try:
-					mac, vendor = line.strip().split("(hex)")
-				except:
-					mac = vendor = ''
+    with open(OUI_FILE) as infile:
+        for line in infile:
+            if re.search("(hex)", line):
+                try:
+                    mac, vendor = line.strip().split("(hex)")
+                except:
+                    mac = vendor = ''
 
-				n = '\t"%s": ' % mac.strip().replace("-",":").lower()
-				n += '"%s",\n' % vendor.strip().replace("'","`")
-				f.write(n)
+                n = '\t"%s": ' % mac.strip().replace("-", ":").lower()
+                n += '"%s",\n' % vendor.strip().replace("'", "`")
+                f.write(n)
 
-	f.write('}')
+    f.write('}')
 
-	# close file
-	f.close()
-	print "\noui.py updated"
+    # close file
+    f.close()
+    print("\noui.py updated")
 
-	print "Removing temportal file"
-	# Remove downloaded file
-	os.remove(OUI_FILE)
+    print("Removing temportal file")
+    # Remove downloaded file
+    os.remove(OUI_FILE)
 
-	print "Done"
+    print("Done")
