f2fd78555b1680f6cf62f83709dfd1980ddc0706
Mikel Sanchez <mikelsmartinez@aol.com>
2020-02-26 09:55:00 +0000

    take default_help to separate file

--- macvendorupdate/__main__.py
+++ macvendorupdate/__main__.py
@@ -1,19 +1,9 @@
-from macvendorToMysql import updateMysql
-from macVendorToPython import updatePython
 import click
 
-class DefaultHelp(click.Command):
-    def __init__(self, *args, **kwargs):
-        context_settings = kwargs.setdefault('context_settings', {})
-        if 'help_option_names' not in context_settings:
-            context_settings['help_option_names'] = ['-h', '--help']
-        self.help_flag = context_settings['help_option_names'][0]
-        super(DefaultHelp, self).__init__(*args, **kwargs)
+from modules.default_help import DefaultHelp
+from macvendorToMysql import updateMysql
+from macVendorToPython import updatePython
 
-    def parse_args(self, ctx, args):
-        if not args:
-            args = [self.help_flag]
-        return super(DefaultHelp, self).parse_args(ctx, args)
 
 @click.command(cls=DefaultHelp)
 @click.option('-p', '--python', is_flag=True, help='Updates/generates python oui file with mac vendor info')
