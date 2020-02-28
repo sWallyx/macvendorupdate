8536851fd833c09cd016882536e52761d3d0230a
Mikel Sanchez <mikelsmartinez@aol.com>
2020-02-28 14:11:20 +0000

    move MySQL errors

--- macvendorupdate/modules/database_settings.py
+++ macvendorupdate/modules/database_settings.py
@@ -2,7 +2,15 @@ from getpass import getpass
 import mysql.connector
 import sys
 
-from global_values import MYSQL_ERROR_MESSAGES
+
+MYSQL_ERROR_MESSAGES = {
+    1036: "I don't know how to write in here, I can only read this table",
+    1037: "Server is about to explode, no memory left, restart it",
+    1045: "I did not get access with, do I have the correct ID? Check user and pass",
+    1049: "For real? That database is not in here. Stop giving me bad directions",
+    1051: "There is no such table. Please don't make fun of me",
+    2003: "Where is it? I cant find the database on that host",
+}
 
 class Database_settings():
 
