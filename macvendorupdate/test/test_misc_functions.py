9c14a2f416a1364ddf57421a46be08bea13f6e92
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-04 10:01:50 +0000

    first things with python pytest

---
+++ macvendorupdate/test/test_misc_functions.py
@@ -0,0 +1,9 @@
+from macvendorupdate.misc_functions import getValuesFromLine
+
+ @pytest.mark.parametrize(("complete_string","expected_mac","expected_vendor"),("string", "mac", "vendor"))
+def test_getValuesFromLine():
+    text_string = ""
+    mac, vendor = getValuesFromLine()
+
+    assert mac = expected_mac
+    assert vendor = expected_vendor
