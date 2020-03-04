669709c695b401c05db322903916b43a1792ad1a
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-04 13:45:26 +0000

    fix test

--- macvendorupdate/test/test_misc_functions.py
+++ macvendorupdate/test/test_misc_functions.py
@@ -1,9 +1,10 @@
+import pytest
 from macvendorupdate.misc_functions import getValuesFromLine
 
-@pytest.mark.parametrize(("complete_string","expected_mac","expected_vendor"),("string", "mac", "vendor"))
-def test_getValuesFromLine():
-    text_string = ""
-    mac, vendor = getValuesFromLine()
+@pytest.mark.parametrize(("complete_string","expected_mac","expected_vendor"),("08-61-95   (hex)		Rockwell Automation", "08-61-95", "Rockwell Automation"))
+def test_getValuesFromLine(complete_string, expected_mac, expected_vendor):
 
-    assert mac = expected_mac
-    assert vendor = expected_vendor
+    mac, vendor = getValuesFromLine(complete_string)
+
+    assert mac == expected_mac
+    assert vendor == expected_vendor
