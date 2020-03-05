dd37354d81d89c1d7782a743a147f549efe7d828
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-05 11:31:23 +0000

    relocate imports

--- macvendorupdate/test/test_misc_functions.py
+++ macvendorupdate/test/test_misc_functions.py
@@ -1,5 +1,5 @@
-import pytest
 from macvendorupdate.misc_functions import getValuesFromLine
+import pytest
 
 @pytest.mark.parametrize(("complete_string","expected_mac","expected_vendor"),("08-61-95   (hex)		Rockwell Automation", "08-61-95", "Rockwell Automation"))
 def test_getValuesFromLine(complete_string, expected_mac, expected_vendor):
