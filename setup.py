b6d1f88f93b71d216b2e5ca0daca54178c6ae9e7
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-05 13:18:43 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='macvendorupdate',
-    version='0.2.5',
+    version='0.2.6',
     description='Scripts to update the mac address brand list to a Python file or a MySQL DataBase',
     author='sWallyx',
-    keywords=['mac address', 'wifi', 'brands', 'people', 'oui', 'devices', 'python', 'mysql'],
+    keywords=['mac address', 'wifi', 'brands', 'people', 'oui', 'devices', 'python', 'mysql', 'download'],
     classifiers=[],
-    install_requires=["click", "mysql-connector-python"],
+    install_requires=["click", "mysql-connector-python", "pytest"],
     setup_requires=[],
     tests_require=[],
+    packages=find_packages(),
 )
