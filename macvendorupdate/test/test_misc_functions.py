da17107267067008a6966cc40f0696c45fb02f99
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-16 15:16:53 +0000

    fix test import

--- macvendorupdate/test/test_misc_functions.py
+++ macvendorupdate/test/test_misc_functions.py
@@ -4,7 +4,7 @@
 """
 import pytest
 
-from misc_functions import get_values_from_line, replace_and_concat
+from macvendorupdate.misc_functions import get_values_from_line, replace_and_concat
 
 
 @pytest.mark.parametrize(
