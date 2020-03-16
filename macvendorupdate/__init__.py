3bc663bc55218091f6c69b3c9d1a15b523917ae9
Mikel Sanchez <mikelsmartinez@aol.com>
2020-03-16 15:16:30 +0000

    fix init files

--- macvendorupdate/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    Init file for imports
-"""
-from .global_values import *
-from .misc_functions import *
-from .to_mysql import *
-from .to_python import *
--- macvendorupdate/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-    Modules init file
-"""
-from .database_actions import *
-from .database_settings import *
